a72723c3a57b4475900299d53981d611a9bc7aa4
Alex Prudencio <alex.prudencio@gmail.com>
2017-01-17 20:25:36 -0500

    send registration

--- motdujour/src/app/app.component.ts
+++ motdujour/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Push, Splashscreen } from 'ionic-native';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 import { TabsPage } from '../pages/tabs/tabs';
-import {TranslateService} from 'ng2-translate';
+import { TranslateService } from 'ng2-translate';
 
 @Component({
   templateUrl: 'app.html'
@@ -11,7 +12,9 @@ import {TranslateService} from 'ng2-translate';
 export class MyApp {
   rootPage = TabsPage;
 
-  constructor(platform: Platform, translate: TranslateService) {
+  constructor(platform: Platform,
+              public http: Http,
+              translate: TranslateService) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -30,6 +33,12 @@ export class MyApp {
 
         push.on('registration', (data)=> {
           console.log('onRegistration', data);
+          let headers = new Headers({ 'Content-Type': 'application/json' });
+          let options = new RequestOptions({ headers: headers });
+          this.http.post('https://motdujour.monolitosoft.com/api/_register', data, options)
+          .subscribe(r => {
+            console.log(r);
+          });
         });
 
         push.on('notification', (data) => {
